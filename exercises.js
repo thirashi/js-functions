81fd19cb2d17e22c924c88d0108fd7070b1c9dd2
Tyler Hirashima <tylerhirashima@Tylers-MBP-5.gateway.ht.net>
2018-02-08 22:05:56 -1000

    Finished Ex18

--- exercises.js
+++ exercises.js
@@ -391,7 +391,23 @@ console.log(canDrink);
  * Function - throwParty
  * Create a function named throwParty. This function will check the value stored in the `canDrink` variable in the previous exercise. If the value is true, it will return the message "Cheee Hoo! We going to da party!" If false, then it will return the message "Meh, see you at Starbucks." Store the return value to a variable named `canParty`. Console.log your result.
  */
+function throwParty() {
 
+if  (canDrink===true) {
+
+return "Chee Hoo! We going to da party!";
+
+}else{
+
+	return "Meh, see you at Starbucks";
+
+}
+
+
+}
+
+var canParty = throwParty();
+console.log(canParty);
 
 
 
